8295b19140a6863ff72694fa4cdcda5b9e568435
zhishaoju <1114082241@qq.com>
2020-05-31 22:48:00 +0800

    little.

--- app/src/main/java/com/youyu/activity/RegisterActivity.java
+++ app/src/main/java/com/youyu/activity/RegisterActivity.java
@@ -27,67 +27,74 @@ import org.json.JSONObject;
  */
 public class RegisterActivity extends BaseActivity {
 
-    private String TAG = RegisterActivity.class.getSimpleName();
+  private String TAG = RegisterActivity.class.getSimpleName();
 
-    @BindView(R.id.et_phone)
-    EditText etPhone;
-    @BindView(R.id.bt_login)
-    Button btLogin;
-    @BindView(R.id.fl_chat)
-    FrameLayout flChat;
-    @BindView(R.id.fl_qq)
-    FrameLayout flQq;
-    @BindView(R.id.cb_protocol)
-    CheckBox cbProtocol;
+  @BindView(R.id.et_phone)
+  EditText etPhone;
+  @BindView(R.id.bt_login)
+  Button btLogin;
+  @BindView(R.id.fl_chat)
+  FrameLayout flChat;
+  @BindView(R.id.fl_qq)
+  FrameLayout flQq;
+  @BindView(R.id.cb_protocol)
+  CheckBox cbProtocol;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        ButterKnife.bind(this);
-        initListener();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_register);
+    ButterKnife.bind(this);
+    initListener();
+  }
 
-    private void initListener() {
-        setNetListener(new RequestResponse() {
-            @Override
-            public void failure(Exception e) {
-                LogUtil.showELog(TAG, "setNetListener check code e :" + e.getLocalizedMessage());
-            }
+  private void initListener() {
+    setNetListener(new RequestResponse() {
+      @Override
+      public void failure(Exception e) {
+        LogUtil.showELog(TAG, "setNetListener failure check code e :" + e.getLocalizedMessage());
+      }
 
-            @Override
-            public void success(String data) {
-                Intent intent = new Intent(RegisterActivity.this, RegisterSetPassActivity.class);
-                intent.putExtra("mobile", etPhone.getText().toString());
-                startActivity(intent);
-            }
-        });
-    }
+      @Override
+      public void success(String data) {
+        LogUtil.showELog(TAG, "setNetListener success data :" + data);
+//        Intent intent = new Intent(RegisterActivity.this, RegisterSetPassActivity.class);
+//        intent.putExtra("mobile", etPhone.getText().toString());
+//        startActivity(intent);
+      }
+    });
+  }
 
-    @OnClick({R.id.bt_login, R.id.cb_protocol})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.bt_login:
-                if (cbProtocol.isChecked()) {
-                    String url = BASE_URL + CODE;
-                    JSONObject jsonObject = new JSONObject();
-                    try {
-                        LogUtil.showDLog(TAG, "R.id.bt_login code :" +  Utils.getIMEI());
-                        jsonObject.put("mobile", etPhone.getText().toString());
-                        jsonObject.put("imei", Utils.getIMEI());
-                    } catch (JSONException e) {
-                        LogUtil.showELog(TAG, "R.id.bt_login e :" + e.getLocalizedMessage());
-                    }
-                    String param = jsonObject.toString();
-                    post(url, param);
-                } else {
-                    Utils.show("请同意相关的协议~");
-                }
-                break;
-            case R.id.cb_protocol:
-                break;
-            default:
-                break;
+  @OnClick({R.id.bt_login, R.id.cb_protocol})
+  public void onViewClicked(View view) {
+    switch (view.getId()) {
+      case R.id.bt_login:
+        if (cbProtocol.isChecked()) {
+          String url = BASE_URL + CODE;
+          JSONObject jsonObject = new JSONObject();
+          try {
+            LogUtil.showDLog(TAG, "R.id.bt_login code :" + Utils.getIMEI());
+            jsonObject.put("mobile", etPhone.getText().toString());
+            jsonObject.put("imei", Utils.getIMEI());
+          } catch (JSONException e) {
+            LogUtil.showELog(TAG, "R.id.bt_login e :" + e.getLocalizedMessage());
+          }
+          String param = jsonObject.toString();
+          post(url, param);
+
+          finish();
+          
+          Intent intent = new Intent(RegisterActivity.this, RegisterSetPassActivity.class);
+          intent.putExtra("mobile", etPhone.getText().toString());
+          startActivity(intent);
+        } else {
+          Utils.show("请同意相关的协议~");
         }
+        break;
+      case R.id.cb_protocol:
+        break;
+      default:
+        break;
     }
+  }
 }
